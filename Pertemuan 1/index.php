25c22ca0cb25bc33d1188db9639b808b7b5b8353
Egirahayu <mohammadegirahayu>
2021-02-07 13:44:38 +0700

    menambahkan folder pertemuan

---
+++ Pertemuan 1/index.php
@@ -0,0 +1,11 @@
+<?php
+    /* 
+        Mohamad Egi Rahayu
+        203040045
+        github.com/Egirahayu
+        Pertemuan 1 (7 Februari 2021)
+        Menjelaskan tentang PHP Dasar
+    */
+
+    echo "Hello World";
+?>
---
+++ Pertemuan 2/index.php
@@ -0,0 +1,80 @@
+<?php
+    /* 
+        Mohamad Egi Rahayu
+        203040045
+        github.com/Egirahayu
+        Pertemuan 2 (7 Februari 2021)
+        Menjelaskan tentang PHP Dasar - Sintaks PHP
+
+        Standar Output
+        echo, print
+        print_r
+        var_dump
+
+        Penulisan Sintaks PHP :
+        1. PHP di dalam HTML
+        2. HTML di dalam PHP
+    */
+
+    // Variabel dan Tipe Data :
+    // Variabel : tidak boleh diawali dengan angka, tetapi boleh mengandung angka
+    $nama = "Mohamad Egi Rahayu";
+    echo "Halo, nama saya $nama";
+
+    // Operator
+    // 1. Aritmatika : + - * / %
+    // $x = 10;
+    // $y = 20;
+    // echo $x * $y;
+
+    // 2. Penggabung string / concatenation / concat : .
+    // $nama_depan = "Egi";
+    // $nama_belakang = "Rahayu";
+    // echo $nama_depan ." ". $nama_belakang;
+
+    // 3. Assigment : =, +=, -=, *=, /=, %=, .=
+    // $x = 1;
+    // $x -= 5;
+    // echo $x;
+
+    // $nama = "Egi";
+    // $nama .= " ";
+    // $nama .= "Rahayu";
+    // echo $nama;
+
+    // 4. Perbandingan : <, >, <=, >=, ==
+    // var_dump(1 < 5);
+    // var_dump(1 > 5);
+    // var_dump(1 == "1");
+
+    // 5. Identitas : ===,!==
+    // var_dump(1 === "1");
+
+    // 6. Logika : &&, ||, !
+    // $x = 10;
+    // var_dump($x < 20 && $x % 2 == 0);
+
+    // $x = 30;
+    // var_dump($x < 20 && $x % 2 == 0);
+
+    // $x = 30;
+    // var_dump($x < 20 || $x % 2 == 0);
+?>
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Belajar PHP</title>
+</head>
+<body>
+    1. PHP di dalam HTML -->
+    <h1>Halo, Selamat Datang <?php echo "Egi Rahayu"; ?></h1>
+    <p><?php echo "ini adalah paragraf"; ?></p>
+
+    <!-- 2. HTML di dalam PHP -->
+    <!-- <?php 
+        echo "<h1>Halo, Selamat Datang Egi Rahayu</h1>";
+    ?>
+</body>
+</html> -->
