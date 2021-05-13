988d50c0fe70a9fd5f4a7e183b42b721742fadd2
Egirahayu <mohammadegirahayu>
2021-05-13 10:57:45 +0700

    Mengubah form login dan registrasi

--- kuliah/pertemuan12/login.php
+++ kuliah/pertemuan12/login.php
@@ -23,82 +23,42 @@ if (isset($_POST['login'])) {
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Login</title>
-  <style>
-    .container {
-      width: 400px;
-      height: 437px;
-      padding-top: 100px;
-    }
-
-    .login {
-      text-align: center;
-      margin-top: 15px;
-    }
-
-    .register {
-      text-align: center;
-    }
-
-    th {
-      text-align: center;
-    }
-
-    .admin {
-      border: 1px solid rgba(0, 0, 0, 1.5);
-      color: white;
-    }
-
-    .remember {
-      margin-left: 5px;
-    }
-
-    marquee {
-      color: white;
-    }
-  </style>
 </head>
 
-<body class="grey darken-4">
-  <marquee class="blue darken-2" behavior="scroll" direction="right">LOGIN & REGISTRASI</marquee>
-
-  <div class="container">
-    <div class="admin green darken-4">
-      <form action="" method="POST">
-        <?php if (isset($login['error'])) : ?>
-          <p style="color: red; font-style: italic;"><?= $login['pesan']; ?></p>
-        <?php endif; ?>
-
-        <table>
-          <h5 class="center">LOGIN</h5>
-          <tr>
-            <td><label for="username">Username</label></td>
-            <td>:</td>
-            <td><input id="username" type="text" name="username" autofocus autocomplete="off" required></td>
-          </tr>
-          <tr>
-            <td><label for="password">Password</label></td>
-            <td>:</td>
-            <td><input id="password" name="password" type="password" required></td>
-          </tr>
-        </table>
-        <div class="login">
-          <button class="waves-effect waves-light blue darken-4 btn" type="submit" name="login">Login</button>
-        </div>
-        <div class="register">
-          <p>Belum punya akun? Registrasi <a href="registrasi.php" class="waves-effect waves-light blue darken-4 btn">Disini</a></p>
-        </div>
-      </form>
-    </div>
-  </div>
-
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+<body>
+  <h3>Form Login</h3>
+  <?php if (isset($login['error'])) : ?>
+    <p style="color: red; font-style:italic;"><?= $login['pesan']; ?></p>
+  <?php endif; ?>
+  <form action="" method="post">
+    <ul>
+      <li>
+        <label>
+          Username :
+          <input type="text" name="username" autofocus autocomplete="off" required>
+        </label>
+      </li>
+      <li>
+        <label>
+          Password :
+          <input type="password" name="password" required>
+        </label>
+      </li>
+      <li>
+        <button type="submit" name="login">Login</button>
+      </li>
+      <li>
+        <a href="registrasi.php">Tambah User Baru</a>
+      </li>
+    </ul>
+  </form>
 </body>
 
 </html>
--- kuliah/pertemuan12/registrasi.php
+++ kuliah/pertemuan12/registrasi.php
@@ -22,78 +22,43 @@ if (isset($_POST['registrasi'])) {
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <link rel="stylesheet" href="css/style.css">
-  <title>Anime</title>
-  <style>
-    .container {
-      width: 400px;
-      height: 437px;
-      padding-top: 100px;
-    }
-
-    .register {
-      text-align: center;
-      margin-top: 15px;
-      margin-bottom: 15px;
-    }
-
-    th {
-      text-align: center;
-    }
-
-    .admin {
-      border: 1px solid rgba(0, 0, 0, 1);
-      color: white;
-    }
-
-    marquee {
-      color: white;
-    }
-  </style>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Registrasi</title>
 </head>
 
-<body class="grey darken-4">
-  <marquee class="blue darken-2" behavior="scroll" direction="right">LOGIN & REGISTRASI</marquee>
+<body>
+  <h3>Form Registrasi</h3>
 
-  <div class="container">
-    <div class="admin green darken-4">
-      <form action="" method="POST">
-        <?php if (isset($error)) : ?>
-          <p style="color: red; font-style: italic;">Username atau Password salah</p>
-        <?php endif; ?>
-
-        <table>
-          <h5 class="center">REGISTRASI</h5>
-          <tr>
-            <td><label for="username">Username</label></td>
-            <td>:</td>
-            <td><input id="username" type="text" name="username" autofocus autocomplete="off" required></td>
-          </tr>
-          <tr>
-            <td><label for="password">Password</label></td>
-            <td>:</td>
-            <td><input id="password" name="password1" type="password" required></td>
-          </tr>
-          <tr>
-            <td><label for="password">Konfirmasi Password</label></td>
-            <td>:</td>
-            <td><input id="password" name="password2" type="password" required></td>
-          </tr>
-        </table>
-        <div class="register">
-          <button class="waves-effect waves-light blue darken-4 btn" type="submit" name="registrasi">Register</button>
-        </div>
-      </form>
-    </div>
-  </div>
-
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  <form action="" method="post">
+    <ul>
+      <li>
+        <label>
+          Username :
+          <input type="text" name="username" autofocus autocomplete="off" required>
+        </label>
+      </li>
+      <li>
+        <label>
+          Password :
+          <input type="password" name="password1" required>
+        </label>
+      </li>
+      <li>
+        <label>
+          Konfirmasi Password :
+          <input type="password" name="password2" required>
+        </label>
+      </li>
+      <li>
+        <button type="submit" name="registrasi">Registrasi</button>
+      </li>
+    </ul>
+  </form>
 </body>
 
 </html>
